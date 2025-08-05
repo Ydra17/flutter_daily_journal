41f5bdc5ba72f7895413c90930fe8d5416d28d86
Ydra17 <ardyantobaso@gmail.com>
2025-08-06 06:41:00 +0800

    feat: implement App widget with light and dark themes for Daily Journal

---
+++ lib/app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_daily_journal/core/theme/dark_theme.dart';
+import 'package:flutter_daily_journal/core/theme/light_theme.dart';
+import 'package:flutter_daily_journal/features/journal/presentation/pages/journal_home_page.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'Daily Journal', theme: lightTheme, darkTheme: darkTheme, themeMode: ThemeMode.system, home: const JournalHomePage());
+  }
+}
