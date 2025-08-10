28876f0cbcf9c352a8da9362059362d8cf74626c
Ydra17 <ardyantobaso@gmail.com>
2025-08-10 14:28:34 +0800

    update journal entity

--- lib/features/journal/domain/entities/journal_entity.dart
+++ lib/features/journal/domain/entities/journal_entity.dart
@@ -1,23 +1,56 @@
-class JournalEntity {
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:equatable/equatable.dart';
+
+import 'package:flutter_daily_journal/features/journal/domain/entities/attachment_entity.dart';
+
+class JournalEntity extends Equatable {
   final String id;
   final DateTime date;
   final String title;
   final String content;
   final bool isFavorite;
+  final List<AttachmentEntity> attachments;
 
-  const JournalEntity({required this.id, required this.date, required this.title, required this.content, this.isFavorite = false});
+  JournalEntity({
+    required this.id,
+    required this.date,
+    required this.title,
+    required this.content,
+    required this.isFavorite,
+    List<AttachmentEntity>? attachments,
+  }) : attachments = attachments ?? [];
+
+  JournalEntity copyWith({
+    String? id,
+    DateTime? date,
+    String? title,
+    String? content,
+    bool? isFavorite,
+    List<AttachmentEntity>? attachments,
+  }) {
+    return JournalEntity(
+      id: id ?? this.id,
+      date: date ?? this.date,
+      title: title ?? this.title,
+      content: content ?? this.content,
+      isFavorite: isFavorite ?? this.isFavorite,
+      attachments: attachments ?? this.attachments,
+    );
+  }
+
+  JournalEntity addAttachment(AttachmentEntity attachment) {
+    return copyWith(attachments: [...attachments, attachment]);
+  }
+
+  JournalEntity removeAttachmentById(String attachmentId) {
+    return copyWith(attachments: attachments.where((a) => a.id != attachmentId).toList());
+  }
 
   @override
-  bool operator ==(Object other) =>
-      identical(this, other) ||
-      other is JournalEntity &&
-          runtimeType == other.runtimeType &&
-          id == other.id &&
-          date == other.date &&
-          title == other.title &&
-          content == other.content &&
-          isFavorite == other.isFavorite;
+  List<Object> get props {
+    return [id, date, title, content, isFavorite, attachments];
+  }
 
   @override
-  int get hashCode => id.hashCode ^ date.hashCode ^ title.hashCode ^ content.hashCode ^ isFavorite.hashCode;
+  bool get stringify => true;
 }
